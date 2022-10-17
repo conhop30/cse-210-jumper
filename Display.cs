deaf3b106706cebc513b6beb1ee253fa26cbb09c
conhop30 <conhop30@gmail.com>
2022-10-16 23:32:09 -0400

    Built Getters and Setters

--- Display.cs
+++ Display.cs
@@ -1,14 +1,27 @@
 //for visuals
 class Display {
+    //initialize class variables
+    Generator generator = new Generator();
+    Guess guess = new Guess();
+
     public void DisplayJumper() {
-        Console.WriteLine(" ___");
-        Console.WriteLine(@"/   \");
-        Console.WriteLine("___");
+        Console.WriteLine(@" ___");
+        Console.WriteLine(@"/___\");
         Console.WriteLine(@"\   /");
         Console.WriteLine(@" \ /");
-        Console.WriteLine("O");
-        Console.WriteLine(@"/|\");
-        Console.WriteLine(@"/ \");
+        Console.WriteLine(@"  O");
+        Console.WriteLine(@" /|\");
+        Console.WriteLine(@" / \");
         Console.WriteLine(@"^^^^^^^");
+    }
+
+    //display the line for the word being guessed
+    //display the letters already guessed
+    public void DisplayWordLine() {
+        for (int i = 0; i != generator.GetWordCount(); i++) {
+            if (generator.GetWord([i]) == guess.GetGuess([i])) {
+
+            }
+        }
     }
 }
